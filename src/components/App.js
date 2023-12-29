02297b1b4a99e923395549292b4638841efea757
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2023-12-29 18:04:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+const[selectedFigure,setSelectedFigure]=useState('square');
+ const[shapesDisplay,setShapesDisplay]=useState(['square','circle']);
+ function appendShape(){
+    setShapesDisplay((prev)=>{
+        return [...prev,selectedFigure]
+    })
+ }
   return (
     <div id="main">
       <div id="shape-creator">
-        
+        <select name=""id=""value={selectedFigure}onChange={(e)=>setSelectedFigure(e.target.value)}>
+                <option value="square">Square</option>
+                <option value="circle">Circle</option>
+          </select>
+          <button onClick={appendShape}>Add Shape</button>
 
       </div>
       <div id="shapes-holder">
+       {
+            shapesDisplay.map((shape,index)=>{
+                return(
+                <div className={shape==='square'?'square':'circle'} key={index}/>
+                )
+          })
+        }
       </div>
     </div>
   )
